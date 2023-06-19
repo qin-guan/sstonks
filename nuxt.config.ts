2fdde8b0c7899c5f980697f2703bee80b41ce117
Qin Guan <qinguan20040914@gmail.com>
2023-06-19 17:39:25 +0800

    feat: add vuefire

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   modules: [
     '@pinia/nuxt',
     '@unocss/nuxt',
+    'nuxt-vuefire'
   ],
   css: [
     'primevue/resources/primevue.css',
@@ -10,5 +11,15 @@ export default defineNuxtConfig({
   ],
   build: {
     transpile: ['primevue']
+  },
+  vuefire: {
+    config: {
+      apiKey: "AIzaSyBYN9qy98CUX2eSgEf54zhTpVn1bF7I3Bg",
+      authDomain: "incredible-sstonks.firebaseapp.com",
+      projectId: "incredible-sstonks",
+      storageBucket: "incredible-sstonks.appspot.com",
+      messagingSenderId: "438193097420",
+      appId: "1:438193097420:web:efb6fab1139930b2479069"
+    }
   }
 })
